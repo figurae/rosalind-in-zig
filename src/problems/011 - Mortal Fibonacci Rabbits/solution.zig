5b7b19fd52ad8990dff36ed983738e8c6ab2f7de
Aleksander Gielnicki <aleksander.gielnicki@gmail.com>
2023-10-01 15:01:44 +0200

    fix: P11 optimize for large numbers

--- src/problems/011 - Mortal Fibonacci Rabbits/solution.zig
+++ src/problems/011 - Mortal Fibonacci Rabbits/solution.zig
@@ -15,42 +15,26 @@ pub fn solution(child_allocator: std.mem.Allocator, input: []const u8) ![]const 
 
     const target_generation = try std.fmt.parseInt(usize, split_input[0], 10);
     const maximum_age = try std.fmt.parseInt(usize, split_input[1], 10);
-    const litter_size = 1;
 
-    var rabbits = std.ArrayList(RabbitPair).init(allocator);
-
-    var first_rabbit_pair: RabbitPair = .{ .age = 0 };
-    try rabbits.append(first_rabbit_pair);
-
-    try countRabbitPairs(allocator, 1, target_generation, litter_size, maximum_age, &rabbits);
-    const rabbitPairs = try std.fmt.allocPrint(child_allocator, "{d}", .{rabbits.items.len});
-
-    return rabbitPairs;
+    const rabbit_pairs = try countRabbitPairs(allocator, target_generation, maximum_age);
+    return try std.fmt.allocPrint(child_allocator, "{d}", .{rabbit_pairs});
 }
 
-fn countRabbitPairs(allocator: std.mem.Allocator, current_gen: usize, target_gen: usize, litter_size: usize, maximum_age: usize, rabbits: *std.ArrayList(RabbitPair)) !void {
-    if (current_gen < target_gen) {
-        var new_rabbits = std.ArrayList(RabbitPair).init(allocator);
-        defer new_rabbits.deinit();
+fn countRabbitPairs(allocator: std.mem.Allocator, target_gen: usize, maximum_age: usize) !u128 {
+    var rabbit_pairs = std.ArrayList(u128).init(allocator);
+    try rabbit_pairs.appendSlice(&[_]u128{ 1, 1 });
 
-        for (rabbits.*.items) |*rabbit_pair| {
-            if (rabbit_pair.age > 0 and rabbit_pair.age < maximum_age - 1) {
-                for (0..litter_size) |_| {
-                    var new_rabbit_pair = .{ .age = 0 };
-                    try new_rabbits.append(new_rabbit_pair);
-                }
-            }
-
-            rabbit_pair.age += 1;
-
-            // NOTE: it's more of a reincarnation but hey, it works!
-            if (rabbit_pair.age >= maximum_age) {
-                rabbit_pair.age = 0;
-            }
+    for (2..target_gen) |i| {
+        var tmp = rabbit_pairs.items[i - 1] + rabbit_pairs.items[i - 2];
+        if (i == maximum_age) {
+            tmp -= 1;
         }
-
-        if (new_rabbits.items.len > 0) try rabbits.appendSlice(new_rabbits.items);
-
-        try countRabbitPairs(allocator, current_gen + 1, target_gen, litter_size, maximum_age, rabbits);
+        if (i > maximum_age) {
+            tmp -= rabbit_pairs.items[i - maximum_age - 1];
+        }
+        try rabbit_pairs.append(tmp);
     }
+
+    const last_index = rabbit_pairs.items.len - 1;
+    return rabbit_pairs.items[last_index];
 }
